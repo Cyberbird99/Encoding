80f8c8e18f2e295e56169b8a0c534bedaac16886
CyberBird <168876396+Cyberbird99@users.noreply.github.com>
2024-12-07 10:47:35 -0600

    Update encodingwith_base64.py

--- encodingwith_base64.py
+++ encodingwith_base64.py
@@ -1,8 +1,3 @@
-"""Text encoding is the process of converting human-readable characters
-into a machine-readable format. 
-Base64 is a binary to a text encoding scheme that represents binary data 
-in an American Standard Code for Information Interchange (ASCII) string format"""
-
 import base64
 
 # First make a function to convert a plain text string to bytes
